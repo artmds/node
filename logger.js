47bb81082883c44334763c330d9dd0db419b6711
artmds <arthur.mdsousa@gmail.com>
2022-10-05 02:12:05 -0300

    end logger

---
+++ logger.js
@@ -0,0 +1,18 @@
+const eventEmitter = require('events')
+const { EventEmitter } = require('stream')
+const fs = require('fs')
+const path =  require('path')
+
+const emitter = new EventEmitter()
+
+emitter.on('log', (message) => {
+  fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
+    if (err) throw err
+  })
+})
+
+function log(message){
+  emitter.emit('log', message)
+}
+
+module.exports = log
